0cf12e8469e2c9279111cb160a1c24365f830d52
PriyadharsiniNS <102780414+PriyadharsiniNS@users.noreply.github.com>
2022-04-11 21:08:27 +0530

    Update delete-institutes.component.ts

--- src/app/delete-institutes/delete-institutes.component.ts
+++ src/app/delete-institutes/delete-institutes.component.ts
@@ -8,7 +8,7 @@ import { MainServiceService } from '../main-service.service';
 })
 export class DeleteInstitutesComponent implements OnInit {
 
-  iid:number=0;
+  instituteId:number=0;
   service:MainServiceService;
   constructor(service:MainServiceService) {
     this.service = service;
@@ -17,9 +17,9 @@ export class DeleteInstitutesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  deleteInsDetails(iid:number)
+  deleteInsDetails(instituteId:number)
   {
-    this.iid = iid;
-    this.service.deleteIns(this.iid).subscribe();
+    this.instituteId = instituteId;
+    this.service.deleteIns(this.instituteId).subscribe();
   }
 }
